234045edffc67a5684dc0b03352895dd910db34d
Banchomin <ban.chomin@gmail.com>
2023-12-28 16:11:34 +0900

    complete

--- app.js
+++ app.js
@@ -5,14 +5,16 @@
 // redo를 클릭하면 이후 값으로 되돌린다.
 // undo와 redo는 동작이 가능할때만 활성화 상태가 된다.
 
-var result = document.getElementById('value');
-var total = 0;
+let total = 0;
+let resultArray = [0];
+let currnetIndex = 0;
 
-var undoButton = document.getElementById('undoButton'),
-  addButton = document.getElementById('addButton'),
-  subButton = document.getElementById('subButton'),
-  redoButton = document.getElementById('redoButton'),
-  inputValue = document.getElementById('inputbox');
+let undoButton = document.getElementById("undoButton"),
+  addButton = document.getElementById("addButton"),
+  subButton = document.getElementById("subButton"),
+  redoButton = document.getElementById("redoButton"),
+  printValue = document.getElementById("value"),
+  inputValue = document.getElementById("inputbox");
 
 // =====  ===== //
 function onload() {
@@ -24,6 +26,68 @@ function onload() {
 
 // ===== handlClick ===== //
 function handleClick(event) {
-  alert(event.target.id);
+  //   alert(event.target.id);
   console.log(event.target.id);
+
+  // const id = event.target.id
+
+  const { id } = event.target;
+  let value, result;
+
+  const calc = (r) => {
+    printValue.innerHTML = r;
+    inputValue.value = 0;
+    currnetIndex++;
+    resultArray = [
+      // 배열을 풀어서 출력 (전개 연산자)
+      ...resultArray.slice(0, currnetIndex + 1),
+      result,
+    ];
+    console.log(`resultArray : ${resultArray}`);
+  };
+
+  switch (id) {
+    case "addButton":
+      /*
+            [기능] 더하기
+            1. 값 입력
+            2. 입력한 값 가져옴
+            3. 입력값 + 기존값
+            4. 출력
+            */
+      value = Number(inputValue.value);
+      result = value + resultArray[currnetIndex];
+      calc(result);
+      break;
+      
+    case "subButton":
+      /*
+            [기능] 빼기
+            1. 값 입력
+            2. 입력한 값 가져옴
+            3. 기존값 - 입력값
+            4. 출력
+            */
+      value = Number(inputValue.value);
+      result = resultArray[currnetIndex] - value;
+      calc(result);
+      break;
+
+    case "undoButton":
+      if (currnetIndex > 0) {
+        currnetIndex = currnetIndex - 1;
+        printValue.innerHTML = resultArray[currnetIndex];
+      } else "최초값입니다.";
+      break;
+
+    case "redoButton":
+      if (currnetIndex < resultArray.length - 1) {
+        currnetIndex = currnetIndex + 1;
+        printValue.innerHTML = resultArray[currnetIndex];
+      } else "마지막값입니다.";
+      break;
+
+    default:
+      return;
+  }
 } // func
